991dbfa5464dae57e0de53827d667c349bef5095
Scott Williams <scott@swilliams.me>
2015-10-10 10:23:55 -0700

    Initial Commit

---
+++ SoundyBoardy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundyBoardy
+//
+//  Created by Scott Williams on 10/10/15.
+//  Copyright © 2015 swilliams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
